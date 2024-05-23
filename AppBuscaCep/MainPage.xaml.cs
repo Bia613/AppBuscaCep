fed21ab5c80676291a94cabbbf048b17e4dce5ab
Usuario <Usuario@CPD02-M10>
2024-05-23 11:40:18 -0300

    continuacao2

--- AppBuscaCep/MainPage.xaml.cs
+++ AppBuscaCep/MainPage.xaml.cs
@@ -17,13 +17,33 @@ namespace AppBuscaCep
         {
             try
             {
-                List<Bairro> arr_bairros = await dataser
+                List<Bairro> arr_bairros = await DataService.GetBairrosByIdCidade(4874);
+
+                //Endereco dados_endereco = await DataService.GetEnderecoByCep(txt_cep.Text);
+
+                //BindingContext = dados_endereco;
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
             }
         }
 
-        private void Button_Clicked_1(object sender, EventArgs e)
+        private async void Button_Clicked_1(object sender, EventArgs e)
         {
+            try
+            {
+                Endereco dados_endereco = await DataService.GetEnderecoByCep(txt_cep.Text); 
 
+                BindingContext = dados_endereco;
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+               
+            }
         }
     }
 
